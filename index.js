decf3f2971d249b35adabb63b8018dd95bca1f67
Kamarudin Samsudin <kamarudin@gmail.com>
2020-01-20 19:52:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
   res.send('Timestamp:'+ ts)
 })
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000...')
+app.listen(8080, function () {
+  console.log('Listening on port 8080...')
 })
